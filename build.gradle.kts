2257861470d941624cb78fe6622b646e0d8a7368
nise_nabe <nise.nabe@gmail.com>
2021-10-09 11:12:36 +0900

    add initial settings for my intellij plugin settings

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import com.nisecoder.gradle.plugin.idea.ext.packagePrefix
+import com.nisecoder.gradle.plugin.idea.ext.settings
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.intellij")
+    id("org.jetbrains.gradle.plugin.idea-ext")
+    id("com.nisecoder.idea-ext-ext")
+}
+
+group = "com.nisecoder.intellij"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+idea {
+    module {
+        settings {
+            packagePrefix["src/main/kotlin"] = "com.nisecoder.intellij.plugins.armeria"
+        }
+    }
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("2021.2.2")
+    type.set("IU")
+    downloadSources.set(true)
+    plugins.set(listOf())
+}
+
+tasks {
+    runIde {
+        autoReloadPlugins.set(true)
+    }
+    patchPluginXml {
+        changeNotes.set("""
+            Add change notes here.<br>
+            <em>most HTML tags may be used</em>        """.trimIndent())
+    }
+}
