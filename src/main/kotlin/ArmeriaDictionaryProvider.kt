8fbd25728cdfc34ff39c5d05b94540b8376a0855
nise_nabe <nise.nabe@gmail.com>
2021-10-31 11:44:18 +0900

    add armeria dictionaries

---
+++ src/main/kotlin/ArmeriaDictionaryProvider.kt
@@ -0,0 +1,9 @@
+package com.nisecoder.intellij.plugins.armeria
+
+import com.intellij.spellchecker.BundledDictionaryProvider
+
+class ArmeriaDictionaryProvider: BundledDictionaryProvider {
+    override fun getBundledDictionaries(): Array<String> {
+        return arrayOf("/dictionaries/armeria.dic")
+    }
+}
