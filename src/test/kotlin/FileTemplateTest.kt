ca19577b34279d86e83e6593952ad90a0921f736
nise_nabe <nise.nabe@gmail.com>
2022-04-09 14:01:15 +0900

    add generated build.gradle.kts test

--- src/test/kotlin/FileTemplateTest.kt
+++ src/test/kotlin/FileTemplateTest.kt
@@ -1,6 +1,13 @@
 package com.linecorp.intellij.plugins.armeria
 
-import com.linecorp.intellij.plugins.armeria.utils.writeKotlin
+import com.intellij.ide.fileTemplates.FileTemplateManager
+import com.intellij.ide.starters.local.StarterUtils
+import com.intellij.openapi.project.ex.ProjectManagerEx
+import com.intellij.openapi.util.JDOMUtil
+import com.intellij.testFramework.OpenProjectTaskBuilder
+import com.intellij.testFramework.TestApplicationManager
+import io.mockk.every
+import io.mockk.mockk
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.jupiter.api.BeforeEach
@@ -25,9 +32,24 @@ internal class FileTemplateTest {
 
     @Test
     fun test() {
-        buildFile.writeKotlin("""
-        """.trimIndent())
+        TestApplicationManager.getInstance()
+        val projectOptions = OpenProjectTaskBuilder().projectName(this.javaClass.name)
+        val intellijProject = ProjectManagerEx.getInstanceEx().openProject(testProjectDir.toPath(), projectOptions.build())
+        val ftManager = FileTemplateManager.getInstance(intellijProject!!)
 
+        val pom = FileTemplateTest::class.java.classLoader.getResourceAsStream("starters/armeria.pom").let { JDOMUtil.load(it) }
+        val dependencyConfig = StarterUtils.parseDependencyConfig(pom, "", true)
+        val template = ftManager.getJ2eeTemplate("armeria-build.gradle.kts")
+        buildFile.writeText(template.getText(mapOf(
+            "context" to mockk<GeneratorContext> {
+                every { hasLanguage("kotlin") } returns true
+                every { hasLanguage("scala") } returns false
+                every { getVersion(any(), any()) } answers {
+                    dependencyConfig.getVersion(arg(0), arg(1))
+                }
+                every { hasLibrary(any()) } returns true
+            },
+        )))
 
         val buildResult = GradleRunner.create()
             .withProjectDir(testProjectDir)
@@ -38,5 +60,21 @@ internal class FileTemplateTest {
         val taskResult = buildResult.task(":help")
         assertNotNull(taskResult)
         assertEquals(TaskOutcome.SUCCESS, taskResult.outcome)
+    }
+
+    /** @see  com.intellij.ide.starters.local.GeneratorContext */
+    @Suppress("unused")
+    interface GeneratorContext {
+        fun hasLanguage(languageId: String): Boolean
+        fun hasLibrary(libraryId: String): Boolean
+        fun hasAnyLibrary(vararg ids: String): Boolean
+        fun hasAllLibraries(vararg ids: String): Boolean
+        fun getVersion(group: String, artifact: String): String?
+        fun getBomProperty(propertyId: String): String?
+        fun getProperty(propertyId: String): String?
+        fun asPlaceholder(propertyId: String): String
+        fun isSdkAtLeast(version: String): Boolean
+        val rootPackagePath: String
+        val sdkFeatureVersion: Int
     }
 }
